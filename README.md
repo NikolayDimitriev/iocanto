bc399734abca1c383c2c969e969d426181df5029
Nikolay Dimitriev <dimitrievk56@mail.ru>
2021-11-10 20:03:05 +0300

    Сверстана главная страница сайта

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # iocanto
 Сайт для музыкальной студии
+
+iocanto16.ru
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script><script type="text/javascript">jQuery(document).ready(function(){jQuery.getJSON('https://iocanto.s20.online/common/2/form/embed?id=2&lead_status_id=&lead_source_id=3',function(data){jQuery('.alfacrm-form-2').replaceWith(data.form)})});</script><span class="alfacrm-form-2"></span><style>.alfacrm-label label {color: #666} .alfacrm-control input[type=text],.alfacrm-control textarea,.alfacrm-control select {padding: 5px;color: #000;width: 99%} .alfacrm-error {color: red;margin-bottom: 15px;font-size: 13px} .alfacrm-submit {padding: 5px;color: #000;width: 100%;font-size: 16px}</style>
---
+++ js/main.js
@@ -0,0 +1,40 @@
+jQuery(document).ready(function(){
+    // !Интеграция с CRM
+    jQuery.getJSON('https://iocanto.s20.online/common/2/form/embed?id=2&lead_status_id=&lead_source_id=3',function(data){
+        jQuery('.alfacrm-form-2').replaceWith(data.form)
+    })
+
+    setTimeout(()=>{
+        // HEADER
+        const header = document.querySelector('.header');
+
+        const headerForm = header.querySelector('form');
+        
+        headerForm.querySelectorAll('.alfacrm-control').forEach(item => {
+            item.classList.add('header-form__block');
+            item.children[0].classList.add('header-form__input');
+        });
+
+        const inputs = headerForm.querySelectorAll('.header-form__input');
+        inputs[0].placeholder = 'Имя'
+        inputs[1].placeholder = 'Телефон'
+
+        const blocks = headerForm.querySelectorAll('.header-form__block');
+        blocks[2].style.display = 'none';
+
+        const button = headerForm.querySelector('button');
+        button.classList.add('header-form__button');
+        button.textContent = 'Записаться на занятие';
+
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <label class="label">
+                <input type="checkbox" class="checkbox"> 
+                <span class="fake"></span>
+                <span class="text">Я соглашаюсь с <a href="#" style="text-decoration: underline;">условиями хранения персональных данных</a></span>
+                
+            </label>
+        `;
+        blocks[1].appendChild(div);
+    }, 200)
+});
